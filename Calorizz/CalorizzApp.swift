52912e103eb7b5012881a263e219d48315eb994d
Santo Martogi <samsecretadmirer2@gmail.com>
2025-06-26 14:50:27 +0700

    Add App

---
+++ Calorizz/CalorizzApp.swift
@@ -0,0 +1,20 @@
+//
+//  CalorizzApp.swift
+//  Calorizz
+//
+//  Created by Foundation-023 on 25/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CalorizzApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
